594eac9bfa132cd060ffd01f4bccadc7ef5c7afc
Ishita Swarnkar <ishitaswarnkar1804@gmail.com>
2026-01-08 20:55:42 +0530

    valid_parentheses.py

---
+++ strings/valid_parentheses.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isValid(self, s):
+        stack = []
+        mapping = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        }
+
+        for char in s:
+            if char in mapping:
+                if not stack or stack[-1] != mapping[char]:
+                    return False
+                stack.pop()
+            else:
+                stack.append(char)
+
+        return not stack
